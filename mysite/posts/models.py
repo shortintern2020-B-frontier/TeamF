c75082935700529fc59387470e120fea2faeba37
Akutagawa <pbwfh384@gmail.com>
2020-09-04 15:52:26 +0900

    Add Comment model

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Comment(models.Model):
+    # post = models.ForeignKey(Post)
+    # user = models.ForeignKey(User)
+    comment = models.CharField(max_length=200)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
