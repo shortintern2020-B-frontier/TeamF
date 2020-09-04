e1a73ae39600ccfbc213999b428bc1e8aa43b6ab
Akutagawa <pbwfh384@gmail.com>
2020-09-04 15:59:38 +0900

    Add __str__ method to Comment class

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -10,6 +10,9 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.comment
+
 
 class FavComment(models.Model):
     # post = models.ForeignKey(Post)
