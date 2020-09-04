e29e4187ccf35ef823d2cb9e4e965286a352e791
Akutagawa <pbwfh384@gmail.com>
2020-09-04 16:08:43 +0900

    Posting comment endpoint

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def comment_post(request, post_id, user_id, comment):
+    """Posting comment function.
+
+    Author:
+        Masato Umakoshi
+    """
+    return HttpResponse(
+        f"Post id: {post_id}, User id: {user_id}, comment content: {comment}")
