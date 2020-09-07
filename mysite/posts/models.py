7d6415571f2ffa594223609f613745569634425c
Akutagawa <pbwfh384@gmail.com>
2020-09-07 09:40:23 +0900

    Change due to merge

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,39 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-class Comment(models.Model):
-    """Comment model for comment on threads.
-
-    Scheme:
-        comment: varchar(500)
-        created_at: timestamp
-        updated_at: timestamp
-        is_deleted: bool
-
-    Note:
-        is_deleted is `True` means that the comment is deleted.
-
-    Author:
-        Masato Umakoshi
-    """
-    # post = models.ForeignKey(Post)
-    # user = models.ForeignKey(User)
-    comment = models.CharField(max_length=200)
-    is_deleted = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.comment
-
-
-class FavComment(models.Model):
-    # post = models.ForeignKey(Post)
-    # user = models.ForeignKey(User)
-    pass
-
-
 # Takahashi Shunichi
 class User(models.Model):
     nickname = models.CharField(max_length=255)
@@ -85,13 +52,29 @@ class Bookmark(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-# Takahashi Shunichi
 class Comment(models.Model):
+    """Comment model for comment on threads.
+
+    Scheme:
+        comment: varchar(500)
+        created_at: timestamp
+        updated_at: timestamp
+        is_deleted: bool
+
+    Note:
+        is_deleted is `True` means that the comment is deleted.
+
+    Author:
+        Takahashi Shunichi
+    """
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comment
 
 
 # Takahashi Shunichi
