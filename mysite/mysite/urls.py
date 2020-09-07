8327073772f3cc825b7bd20b7d8b993c60876b0f
Akutagawa <pbwfh384@gmail.com>
2020-09-07 09:43:04 +0900

    Bug fix

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import include, path
-=======
-from django.urls import path, include
->>>>>>> origin/post
 
 urlpatterns = [
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
     # path('users/', include('users.urls')),
-    path('posts/', include('posts.urls')),
 ]
