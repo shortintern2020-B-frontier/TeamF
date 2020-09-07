61d0074407313e7d9a54a192756d50762312ba00
Akutagawa <pbwfh384@gmail.com>
2020-09-07 09:45:29 +0900

    execute makemigrations

--- mysite/posts/migrations/0001_initial.py
+++ mysite/posts/migrations/0001_initial.py
@@ -28,6 +28,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Book',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
