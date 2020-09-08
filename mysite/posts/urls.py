72f442f83ed546e5a5642fd4d05034575989701d
Akutagawa <pbwfh384@gmail.com>
2020-09-08 16:07:57 +0900

    Fix quatation

--- mysite/posts/urls.py
+++ mysite/posts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("nice", views.nice_create, name="nice"),
     path("wokashi", views.wokashi_create, name="wokashi"),
     path("ahare", views.ahare_create, name="ahare"),
-    path('bookmark', views.bookmark_create, name='bookmark'),
+    path("bookmark", views.bookmark_create, name='bookmark'),
     path("comment_edit/<int:num>", views.comment_edit, name="comment_edit"),
     path("comment_delete/<int:num>",
          views.comment_delete,
