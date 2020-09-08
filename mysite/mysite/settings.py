6e35ce3110b54ad37e927249aa321645ca425fde
Sota Yamamoto <yamasou23@gmail.com>
2020-09-08 14:43:01 +0900

    ヘッダーメニューのデザイン（Ver.1）と、ヘッダーを親HTMLファイルとして切り出し、全体に適応できるように変更。パス回りの調整など。

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# updated by Yamamoto Sota（TEMPLATES, STATICFILES のパス設定）
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +69,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
