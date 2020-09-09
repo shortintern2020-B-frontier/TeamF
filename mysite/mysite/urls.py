e3ce14cbf5eaff085768231a802ced7e1ba38d49
Akutagawa <pbwfh384@gmail.com>
2020-09-09 09:22:00 +0900

    Merge settings/urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView  
+from app import views
 
 urlpatterns = [
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
     # path('users/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='top.html'), name='home'),
+    path('create_account', views.SignUpView.as_view(), name='create_account')
 ]
