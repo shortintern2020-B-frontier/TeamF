b26fae86d99d36bc41c5ab253704a090d25023f9
Kyohei-Yasunaga <67771237+Kyohei-Yasunaga@users.noreply.github.com>
2020-09-09 14:07:24 +0900

    Update forms.py

--- mysite/app/forms.py
+++ mysite/app/forms.py
@@ -10,7 +10,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2','icon')
 
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
