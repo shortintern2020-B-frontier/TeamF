d9214baa40678a94c5c6e1513bc24fc6ff7e0b48
shung-ich <takahashi.shunichi.83a@st.kyoto-u.ac.jp>
2020-09-10 00:10:41 +0900

    TagFormリファクタリング

--- mysite/posts/forms.py
+++ mysite/posts/forms.py
@@ -7,15 +7,10 @@ class PostForm(forms.Form):
     title = forms.CharField(label='作品名')
     author = forms.CharField(label='著者名')
 
-
+# Takahashi Shunichi
 class TagForm(forms.Form):
     data = [[c.id, c.category] for c in Category.objects.all()]
-    # for c in Category.objects.all():
-    #     data.append([c.id, c.category])
     tag = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=data)
-
-    class Meta:
-        initial = [category.category for category in Category.objects.filter()]
 
 
 class CommentForm(forms.Form):
