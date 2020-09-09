a3fdfa1bc5981e21b8932257c84af228899d106c
Akutagawa <pbwfh384@gmail.com>
2020-09-09 16:31:49 +0900

    Add model

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,33 +1,37 @@
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import redirect, render
 from django.views.generic import CreateView
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
+
 from app.forms import SignUpForm
+from app.models import ImageChoice
+
 
 class SignUpView(CreateView):
     def post(self, request, *args, **kwargs):
 
-        all_data = request.POST
-        form_data = all_data
-        img_choice = all_data['user_img']
-        del form_data['user_img']
+        form_data = request.POST
+        # user_img = form_data["user_img"]
+        # del form_data["user_img"]
 
         form = SignUpForm(data=form_data)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, email=email, password=password,icon=icon)
-            img_choice = ImageChoice(user=user, image_choice=img_choice)
+            username = form.cleaned_data.get("username")
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username,
+                                email=email,
+                                password=password)
+            img_choice = ImageChoice(user=user, url_path="hogehoge")
             img_choice.save()
             login(request, user)
-            return redirect('/')
-        return render(request, 'app/signup.html', {'form': form})
+            return redirect("/")
+        return render(request, "app/signup.html", {"form": form})
 
     def get(self, request, *args, **kwargs):
         form = SignUpForm(request.POST)
-        return render(request, 'app/signup.html', {'form': form})
+        return render(request, "app/signup.html", {"form": form})
 
     def form_valid(self, form):
         user = form.save()
